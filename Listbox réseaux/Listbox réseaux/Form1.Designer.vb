f89fab65fb25a501cd80dec427171ec32b4be61b
Simon-Briaux <113522528+Simon-Briaux@users.noreply.github.com>
2023-05-17 09:12:16 +0200

    ping des différent réseaux

---
+++ Listbox réseaux/Listbox réseaux/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Réseaux = New ListBox()
+        Button1 = New Button()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Réseaux
+        ' 
+        Réseaux.FormattingEnabled = True
+        Réseaux.ItemHeight = 20
+        Réseaux.Location = New Point(12, 12)
+        Réseaux.Name = "Réseaux"
+        Réseaux.Size = New Size(523, 204)
+        Réseaux.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(504, 303)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(204, 80)
+        Button1.TabIndex = 7
+        Button1.Text = "Test psexec"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(541, 12)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(72, 20)
+        Label6.TabIndex = 8
+        Label6.Text = "Tout ping"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label6)
+        Controls.Add(Button1)
+        Controls.Add(Réseaux)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Formulaire extinction PC"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Réseaux As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Listbox réseaux/Listbox réseaux/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Réseaux.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim unused = Shell("C:\Users\Simon\test\psexec.bat")
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+        Réseaux.Items.Clear()
+        'checklan("10.80.15.", 1)
+        'checklan("10.80.24.", 1)
+        'checklan("10.255.85.", 1)
+        'checklan("10.80.13.", 1)
+        checklan("10.80.1.", 1)
+        checklan("10.80.6.", 1)
+        checklan("10.80.102.", 1)
+        'checklan("10.80.15.", 2)
+        'checklan("10.80.24.", 2)
+        'checklan("10.255.85.", 2)
+        'checklan("10.80.13.", 2)
+        checklan("10.80.1.", 2)
+        checklan("10.80.6.", 2)
+        checklan("10.80.102.", 2)
+        ''quiter l'application
+    End Sub
+    Function checklan(lan, état)
+        '''test du réseau sélectionné
+        ''crée une liste de poste a ne pas éteindre (whitelist)
+        Dim count = 0
+        For x = 1 To 10
+            If My.Computer.Network.Ping(lan & x) Then
+                count = count + 1
+                Réseaux.Items.Add(lan & x & " = Server pinged successfully.")
+                ''lancer un web service qui sauvegardera l'indication post allumé dans la base sur le cham état correspondant
+                ''éteindre le PC si il n'appartien pas a la whitlist
+            End If
+        Next
+        If count = 0 Then Réseaux.Items.Add(lan & "0/24s = aucun PC n'as été Ping")
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ''lancer un web service pour réinitialiser les état a vide
+        ''démarer un timer qui lancera au bout de 3 minutes une fonction (la meme que label6_click) penser a éteindre la timer
+    End Sub
+End Class
